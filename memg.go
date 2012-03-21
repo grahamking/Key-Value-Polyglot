babbc22debfe5351f57168671094a141de7dbed4
Graham King <graham@gkgk.org>
2012-03-20 21:54:58 -0700

    Upgraded Go version to Go v1. Ran gofmt on it

--- memg.go
+++ memg.go
@@ -1,62 +1,62 @@
-#!/usr/bin/env gorun
-package main;
+package main
 
 import (
-    "fmt"
-    "net"
-    "bufio"
-    "strings"
-    "os"
-    "strconv"
-    //"runtime/pprof"   // Uncomment to profile
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	//"runtime/pprof"   // Uncomment to profile
 )
 
-var CACHE map[string] string;
+var CACHE map[string]string
 
 func main() {
 
-    // Uncomment these three lines to profile
-    /*
-    handle, _ := os.Create("memg.prof")
-    pprof.StartCPUProfile(handle)
-    defer pprof.StopCPUProfile()
-    */
+	// Uncomment these three lines to profile
+	/*
+	   handle, _ := os.Create("memg.prof")
+	   pprof.StartCPUProfile(handle)
+	   defer pprof.StopCPUProfile()
+	*/
 
-    listener, err := net.Listen("tcp", "127.0.0.1:11211")
-    if err != nil {
-        panic("Error listening on 11211: " + err.String())
-    }
+	listener, err := net.Listen("tcp", "127.0.0.1:11211")
+	if err != nil {
+		panic("Error listening on 11211: " + err.Error())
+	}
 
-    CACHE = make(map[string] string)
+	CACHE = make(map[string]string)
 
-    if isSingle() {
-        netconn, err := listener.Accept()
-        if err != nil {
-            panic("Accept error: " + err.String())
-        }
+	if isSingle() {
+		netconn, err := listener.Accept()
+		if err != nil {
+			panic("Accept error: " + err.Error())
+		}
 
-        handleConn(netconn)
+		handleConn(netconn)
 
-    } else {
-        for {
-            netconn, err := listener.Accept()
-            if err != nil {
-                panic("Accept error: " + err.String())
-            }
+	} else {
+		for {
+			netconn, err := listener.Accept()
+			if err != nil {
+				panic("Accept error: " + err.Error())
+			}
 
-            go handleConn(netconn)
-        }
-    }
+			go handleConn(netconn)
+		}
+	}
 
 }
 
 func isSingle() bool {
-    for _, arg := range os.Args  {
-        if arg == "--single" {
-            return true
-        }
-    }
-    return false
+	for _, arg := range os.Args {
+		if arg == "--single" {
+			return true
+		}
+	}
+	return false
 }
 
 /*
@@ -64,47 +64,47 @@ func isSingle() bool {
  */
 func handleConn(conn net.Conn) {
 
-    reader := bufio.NewReader(conn)
-    for {
+	reader := bufio.NewReader(conn)
+	for {
 
-        // Fetch
+		// Fetch
 
-        content, err := reader.ReadString('\n')
-        if err == os.EOF {
-            break
-        } else if err != nil {
-            fmt.Println(err)
-            return
-        }
+		content, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-        content = content[:len(content) - 2]    // Chop \r\n
+		content = content[:len(content)-2] // Chop \r\n
 
-        // Handle
+		// Handle
 
-        parts := strings.Split(content, " ")
-        cmd := parts[0]
-        switch cmd {
+		parts := strings.Split(content, " ")
+		cmd := parts[0]
+		switch cmd {
 
-        case "get":
-            key := parts[1]
-            val, ok := CACHE[key]
-            if ok {
-                length := strconv.Itoa(len(val))
-                conn.Write([]uint8("VALUE " + key + " 0 " + length + "\r\n"))
-                conn.Write([]uint8(val + "\r\n"))
-            }
-            conn.Write([]uint8("END\r\n"))
+		case "get":
+			key := parts[1]
+			val, ok := CACHE[key]
+			if ok {
+				length := strconv.Itoa(len(val))
+				conn.Write([]uint8("VALUE " + key + " 0 " + length + "\r\n"))
+				conn.Write([]uint8(val + "\r\n"))
+			}
+			conn.Write([]uint8("END\r\n"))
 
-        case "set":
-            key := parts[1]
-            //exp := parts[2]
-            //flags := parts[3]
-            length, _ := strconv.Atoi(parts[4])
-            // Really we should read exactly 'length' bytes + \r\n
-            val := make([]byte, length);
-            reader.Read(val)
-            CACHE[key] = string(val);
-            conn.Write([]uint8("STORED\r\n"))
-        }
-    }
+		case "set":
+			key := parts[1]
+			//exp := parts[2]
+			//flags := parts[3]
+			length, _ := strconv.Atoi(parts[4])
+			// Really we should read exactly 'length' bytes + \r\n
+			val := make([]byte, length)
+			reader.Read(val)
+			CACHE[key] = string(val)
+			conn.Write([]uint8("STORED\r\n"))
+		}
+	}
 }
