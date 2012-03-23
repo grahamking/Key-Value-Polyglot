5c2a6cd6924f4d919075852be51eb5da07a1c06d
zedr <zedr@ipnext.it>
2012-03-23 11:55:30 +0100

    Simplified things

--- memg.js
+++ memg.js
@@ -5,34 +5,6 @@ var handle_data = function (data, sock) {
 
     "use strict";
 
-    /* Process the data, splitting it into parts 
-
-       TODO: improve the splitting part. */
-    var parts = data.split(' ');
-    var cmd = parts[0], key = parts[1], val;
-
-    switch(cmd) {
-        
-        case "get":
-            key = key.split('\r\n')[0];
-            val = CACHE[key];
-            if (val) {
-                var msg = "VALUE " + key + " 0 " + val.length + "\r\n";
-                msg += val + "\r\n";
-                sock.write(msg);
-            }
-            sock.write("END\r\n");
-            break;
-
-        case "set":
-            var tmp = parts[4].split('\r\n');
-            if (tmp[1]) {
-                var length = +tmp[0];
-                CACHE[key] = tmp[1].slice(0, length);
-                sock.write("STORED\r\n");
-            };
-            break;
-    };
 };
 
 var handle_conn = function (sock) {
@@ -46,16 +18,33 @@ var handle_conn = function (sock) {
         /* Upon receiving the socket connection, extract the data */
         data = chunk.toString()
 
-        /* If we have something, we will process it */
-        if (data) handle_data(data, sock);
+        /* If we have something, we will process it
+           TODO: improve the splitting part. */
+        var parts = data.split(' ');
+        var cmd = parts[0], key = parts[1], val;
 
-    });
+        switch(cmd) {
+        
+            case "get":
+                key = key.split('\r\n')[0];
+                val = CACHE[key];
+                if (val) {
+                    var msg = "VALUE " + key + " 0 " + val.length + "\r\n";
+                    msg += val + "\r\n";
+                    sock.write(msg);
+                }
+                sock.write("END\r\n");
+                break;
 
-    /* Wrap up and return the headers */
-    sock.on('end', function () {
-
-        sock.writeHead(200);
-        sock.end();
+            case "set":
+                var tmp = parts[4].split('\r\n');
+                if (tmp[1]) {
+                    var length = +tmp[0];
+                    CACHE[key] = tmp[1].slice(0, length);
+                    sock.write("STORED\r\n");
+                };
+                break;
+        };
 
     });
 
