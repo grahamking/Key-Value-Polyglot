d5fa20af219b049b65b2b0c47d10e30320c996f1
Graham King <graham@gkgk.org>
2012-03-24 18:14:43 -0700

    Idiomatize Go. Thanks @surma

--- memg.go
+++ memg.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"bufio"
+    "flag"
 	"fmt"
 	"io"
 	"net"
-	"os"
 	"strconv"
 	"strings"
 )
 
-var CACHE map[string]string
+var (
+  CACHE      = map[string]string{}
+  singleFlag = flag.Bool("single", false, "Start in single mode")
+)
 
 func main() {
+    flag.Parse()
 
 	listener, err := net.Listen("tcp", "127.0.0.1:11211")
 	if err != nil {
@@ -21,7 +25,7 @@ func main() {
 
 	CACHE = make(map[string]string)
 
-	if isSingle() {
+	if *singleFlag {
 		netconn, err := listener.Accept()
 		if err != nil {
 			panic("Accept error: " + err.Error())
@@ -40,15 +44,6 @@ func main() {
 		}
 	}
 
-}
-
-func isSingle() bool {
-	for _, arg := range os.Args {
-		if arg == "--single" {
-			return true
-		}
-	}
-	return false
 }
 
 /*
